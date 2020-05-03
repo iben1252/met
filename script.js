665a5f71ff6890f84a110e8b7327faeecbd912ae
norw0001 <norw0001@stud.kea.dk>
2020-05-03 13:00:47 +0200

    JavaScript igangsat - section på forsidens galleri

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+	console.log('is touch', is_touch_device());
+
+	//'is touch' - en funktion som kan genkende den enhed man er på; mobil eller desktop. Den bruges netop i forbindelse med forsidens galleri billede som kan flippe frem og tilbage mellem tekst om caféens åbningstider og et billede af caféen.
+
+	document.querySelector('html').classList.remove('is_touch_device', 'is_not_touch_device');
+	//Vi sikrer os at der ikke er nogen klasser på forhånd.
+
+	document.querySelector('html').classList.add(
+		is_touch_device() ? 'is_touch_device' : 'is_not_touch_device');
+	//Funktionen kigger på om det er true eller false ift. hvilken enhed det er.
+
+	destGallery = document.querySelector("#gallery-section");
+
+	async function getGallery() {
+		let pagesURL = "";
+		// Husk at sætte WP post URL ind.
+		let jsonData = await fetch(pagesURL);
+		section = await jsonData.json();
+		insertGallery();
+		//Med InsertGallery, "aktiverer" vi funktionen til at virke.
+
+		function insertGallery() {
+			section.forEach(section => {
+				let template =
+					`
+						<section>
+							<div class="content">
+							<div class="image_content">
+								<img src="${section.billede.guid}" alt="Billede til: ${section.title.rendered}">
+							</div>
+							<div class="gallery_content" id="${section.slug}">
+								<img src="${section.billede.guid}" alt="Billede til: ${section.title.rendered}"><img src="${section.billede.guid}" alt="Billede til: ${section.title.rendered}"><img src="${section.billede.guid}" alt="Billede til: ${section.title.rendered}"><img src="${section.billede.guid}" alt="Billede til: ${section.title.rendered}">
+							</div>
+						</div>
+						</section>
+					`;
+				//Section er strukturen på forsidens billedgalleri - Derfor har klassen, gallery_content, fået fire img srcs, da det er herinde vi skal indsætte billeder fra caféen eller deres mad.
+				destGallery.insertAdjacentHTML("beforeend", template);
+			})
+		}
+
+
+
+	}
+
+}
