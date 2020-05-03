a4c473c9b8966ade0333c557954869ae3591b3a2
Joanna Nguyen <joannanguyen@Joannas-MacBook-Pro.local>
2020-05-03 16:45:06 +0200

    lukke knap script

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
-	console.log('is touch', is_touch_device());
+    console.log('is touch', is_touch_device());
 
-	//'is touch' - en funktion som kan genkende den enhed man er på; mobil eller desktop. Den bruges netop i forbindelse med forsidens galleri billede som kan flippe frem og tilbage mellem tekst om caféens åbningstider og et billede af caféen.
+    //'is touch' - en funktion som kan genkende den enhed man er på; mobil eller desktop. Den bruges netop i forbindelse med forsidens galleri billede som kan flippe frem og tilbage mellem tekst om caféens åbningstider og et billede af caféen.
 
-	document.querySelector('html').classList.remove('is_touch_device', 'is_not_touch_device');
-	//Vi sikrer os at der ikke er nogen klasser på forhånd.
+    document.querySelector('html').classList.remove('is_touch_device', 'is_not_touch_device');
+    //Vi sikrer os at der ikke er nogen klasser på forhånd.
 
-	document.querySelector('html').classList.add(
-		is_touch_device() ? 'is_touch_device' : 'is_not_touch_device');
-	//Funktionen kigger på om det er true eller false ift. hvilken enhed det er.
+    document.querySelector('html').classList.add(
+        is_touch_device() ? 'is_touch_device' : 'is_not_touch_device');
+    //Funktionen kigger på om det er true eller false ift. hvilken enhed det er.
 
 
 
 }
 destGallery = document.querySelector("#gallery-section");
-async function getGallery() {
-	let pagesURL = "http://widespace.dk/MET-WP/wp-json/wp/v2/forside_-_galleri?per_page=100";
-	// Husk at sætte WP post URL ind.
-	let jsonData = await fetch(pagesURL);
-	section = await jsonData.json();
-	insertGallery();
-	//Med InsertGallery, "aktiverer" vi funktionen til at virke.
 
-	function insertGallery() {
-		section.forEach(section => {
-			let template =
-				`
+async function getGallery() {
+    let pagesURL = "http://widespace.dk/MET-WP/wp-json/wp/v2/forside_-_galleri?per_page=100";
+    // Husk at sætte WP post URL ind.
+    let jsonData = await fetch(pagesURL);
+    section = await jsonData.json();
+    insertGallery();
+    //Med InsertGallery, "aktiverer" vi funktionen til at virke.
+}
+
+function insertGallery() {
+    section.forEach(section => {
+        let template =
+            `
 						<section>
 							<div class="content">
 								<div class="billede_venstre">
@@ -54,8 +56,8 @@ async function getGallery() {
 						</div>
 						</section>
 					`;
-			//Section er strukturen på forsidens billedgalleri - Derfor har klassen, gallery_content, fået fire img srcs, da det er herinde vi skal indsætte billeder fra caféen eller deres mad.
-			destGallery.insertAdjacentHTML("beforeend", template);
-		})
-	}
+        //Section er strukturen på forsidens billedgalleri - Derfor har klassen, gallery_content, fået fire img srcs, da det er herinde vi skal indsætte billeder fra caféen eller deres mad.
+        destGallery.insertAdjacentHTML("beforeend", template);
+    })
+}
 }
