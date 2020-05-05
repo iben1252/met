ecd1ff9bc20a9903824ca9070af48abdfad72e9c
Iben Bording Nielsen <ibennielsen@Ibens-MacBook-Air.local>
2020-05-05 10:30:46 +0200

    Quote slider - semi done - Norwin/Iben

--- script.js
+++ script.js
@@ -103,43 +103,6 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-
-
-
-//
-//async function getQuote() {
-//	console.log("quote");
-//	let pagesURL = "http://widespace.dk/MET-WP/wp-json/wp/v2/forside?per_page=100";
-//	let jsonData = await fetch(pagesURL);
-//	section = await jsonData.json();
-//	insertQuote();
-//}
-//
-//function insertQuote() {
-//	section.forEach((section) => {
-//		console.log("get quote");
-//		let template =
-//
-//			`
-//						<section>
-//							<div class="content">
-//								<div class="quote-container" style="width:70vw; height:70vh; overflow-x:auto; display:flex; -webkit-overflow-scrolling:touch; scroll-behavior:smooth;">
-//									<p style="width:70vw; flex-shrink:0; ">${section.citat}</p>
-//
-//									<p style="width:70vw; flex-shrink:0;">${section.citat_2}</p>
-//
-//									<p style="width:70vw; flex-shrink:0;>${section.citat_3}</p>
-//
-//								</div>
-//							</div>
-//						</section>
-//					`;
-//		//Section er strukturen på forsidens billedgalleri - Derfor har klassen, gallery_content, fået fire img srcs, da det er herinde vi skal indsætte billeder fra caféen eller deres mad.
-//		destQuote.insertAdjacentHTML("beforeend", template);
-//	});
-//}
-
-
 //----------------------- Forside Citat SLUT----------------------------
 
 
