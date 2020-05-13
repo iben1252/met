3d8e6c02405c5f7ed3b69a73c3456e4dcdc4831f
Joanna Nguyen <joannanguyen@Joannas-MacBook-Pro.local>
2020-05-13 14:11:13 +0200

    menu

--- menu.js
+++ menu.js
@@ -82,231 +82,70 @@ function insertmenuKort(section) {
     let template =
         `
 
-<div id="kaffe-sektion">
+        <div id="kaffe-sektion">
+                <div class="menu-left">
+                        <p>${section.kaffe_overskrift}</p>
+                        <p>${section.kaffe_beskrivelse}</p>
 
-    <div class="menu-left">
-        <p>${section.kaffe_overskrift}</p>
-        <p>${section.kaffe_beskrivelse}</p>
-
-            <ul>
+                        <ul>
+                            <li>
+                                <p>${section.kaffe_menu}</p>
+                            </li>
                 <li>
-                    <p>${section.kaffe_menu}</p>
-                    <p>${section.kaffe_priser}</p>
-                </li>
-            </ul>
-    </div>
+                                <p>${section.kaffe_priser}</p> </li>
+                        </ul>
+                    </div>
 
-    <div class="menu-right">
-        <img src="${section.kaffebillede.guid}" alt="Billede til: ${section.title.rendered}";">
-    </div>
+                <div class="menu-right">
+                        <img src="${section.kaffebillede.guid}" alt="Billede til: ${section.title.rendered}";">
+                </div>
 
-</div>
+                </div>
 
-<div id="morgenmad-sektion">
+            <div id="morgenmad-sektion">
+                <div class="menu-left">
+                        <p>${section.morgenmad_overskrift}</p>
+                        <p>${section.morgenmad_beskrivelse}</p>
 
-    <div class="menu-left">
-        <p>${section.morgenmad_overskrift}</p>
-        <p>${section.morgenmad_beskrivelse}</p>
+                        <ul>
+                            <li>
+                                <p>${section.morgenmad_menu}</p>
+                                <p>${section.morgenmad_priser}</p>
+                            </li>
+                        </ul>
+                </div>
 
-            <ul>
-                <li>
-                    <p>${section.morgenmad_menu}</p>
-                    <p>${section.morgenmad_priser}</p>
-                </li>
-            </ul>
-    </div>
+                <div class="menu-right">
+                    <img src="${section.morgenmadbillede.guid}" alt="Billede til: ${section.title.rendered}";">
+                </div>
 
-    <div class="menu-right">
-        <img src="${section.morgenmadbillede.guid}" alt="Billede til: ${section.title.rendered}";">
-    </div>
-
-</div>
+            </div>
 
 
-<div id="koldedrikke-sektion">
+            <div id="koldedrikke-sektion">
 
-    <div class="menu-left">
-        <p>${section.kolde_drikke}</p>
-        <p>${section.kolde_beskrivelse}</p>
+                <div class="menu-left">
+                    <p>${section.kolde_drikke}</p>
+                    <p>${section.kolde_beskrivelse}</p>
 
-            <ul>
-                <li>
-                    <p>${section.kolde_menu}</p>
-                    <p>${section.kolde_priser}</p>
-                </li>
-            </ul>
-    </div>
+                <ul>
+                    <li>
+                        <p>${section.kolde_menu}</p>
+                        <p>${section.kolde_priser}</p>
+                    </li>
+                </ul>
+            </div>
 
-    <div class="menu-right">
-        <img src="${section.koldebillede.guid}" alt="Billede til: ${section.title.rendered}";">
-    </div>
+            <div class="menu-right">
+                <img src="${section.koldebillede.guid}" alt="Billede til: ${section.title.rendered}";">
+            </div>
 
-</div>
-
-
-
-
+        </div>
 
 					`;
+
     destmenuKort.insertAdjacentHTML("beforeend", template);
-
 }
-
-
-//<div id="sectionwrapper">
-//        <div id="section">
-//            <div class="menu-left">
-//                <div class="coffee-text">
-//                    <h1>Kaffe</h1>
-//                    <p>PÅ RYESGADE 25 ligger vores hyggelige lille sted, MET. Her kan man få en sublim kop kaffe, morgenmad, salater & sandwiches der mætter både maven & øjet.</p>
-//                    <ul>
-//                        <li>
-//                            <p>Espresso</p>
-//                            <p>25 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Americano</p>
-//                            <p>30 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Cortado</p>
-//                            <p>34 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Flat white</p>
-//                            <p>38 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Cappuccino</p>
-//                            <p>38 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Latte</p>
-//                            <p>38 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Iskaffe</p>
-//                            <p>38 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Filter</p>
-//                            <p>25 / 30 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Te</p>
-//                            <p>30 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Varm kakao</p>
-//                            <p>38 kr.</p>
-//                        </li>
-//                    </ul>
-//                </div>
-//            </div>
-//            <div class="menu-right">
-//                <div class="coffee-photo"></div>
-//            </div>
-//        </div>
-//
-//        <div id="section">
-//            <div class="menu-left">
-//                <div class="food-photo">
-//                </div>
-//            </div>
-//            <div class="menu-right">
-//                <div class="food-text">
-//                    <h1>Morgenmad og brunch</h1>
-//                    <p>PÅ RYESGADE 25 ligger vores hyggelige lille sted, MET. Her kan man få en sublim kop kaffe, morgenmad, salater & sandwiches der mætter både maven & øjet.</p>
-//                    <ul>
-//                        <li>
-//                            <div class="surdejs-upper">
-//                                <p>Surdejsbolle eller tebolle</p>
-//                                <p>17 kr.</p>
-//                            </div>
-//                            <div class="surdejs-lower">
-//                                <p>- med smør & ost</p>
-//                                <p>32 kr.</p>
-//                            </div>
-//                        </li>
-//                        <li>
-//                            <p>Yoghurt med granola & kompot</p>
-//                            <p>40 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Overnight oats</p>
-//                            <p>40 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Focaccia</p>
-//                            <p>25 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Grape m. mynte/basilikum sukker</p>
-//                            <p>20 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Chokostykke</p>
-//                            <p>20 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Æg m. urtesalt & chili</p>
-//                            <p>20 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Månedens kage</p>
-//                            <p>35 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Månedens salater</p>
-//                            <p>38 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Sandwich</p>
-//                            <p>38 kr.</p>
-//                        </li>
-//                    </ul>
-//                    <p>Ekstra bagværk fredag, lørdag & søndag</p>
-//                    <ul>
-//                        <li>
-//                            <p>Kanelsnurre eller stickybuns</p>
-//                            <p>25 kr.</p>
-//                        </li>
-//                    </ul>
-//                </div>
-//            </div>
-//        </div>
-//
-//        <div id="section">
-//            <div class="menu-left">
-//                <div class="drinks-text">
-//                    <h1>Kolde drikke</h1>
-//                    <p>PÅ RYESGADE 25 ligger vores hyggelige lille sted, MET. Her kan man få en sublim kop kaffe, morgenmad, salater & sandwiches der mætter både maven & øjet.</p>
-//                    <ul>
-//                        <li>
-//                            <p>GRO juice</p>
-//                            <p>40 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Lemonade</p>
-//                            <p>30 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Kombucha</p>
-//                            <p>35 kr.</p>
-//                        </li>
-//                        <li>
-//                            <p>Cortado</p>
-//                            <p>34 kr.</p>
-//                        </li>
-//                    </ul>
-//                </div>
-//            </div>
-//            <div class="menu-right">
-//                <div class="drinks-photo"></div>
-//            </div>
-//
-//        </div>
-//    </div>
 
 //---------------------------------Menu sektion SLUT ----------------------------
 
