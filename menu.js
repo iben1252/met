1e65c825d25e75959147eebe5e59997db7f3511d
Joanna Nguyen <joannanguyen@Joannas-MacBook-Pro.local>
2020-05-15 09:51:28 +0200

    Json inde i JS menu - Joanna

--- menu.js
+++ menu.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
-	burgerStart();
-	getFooter1();
-	getFooter2();
-	menuKort()
+    burgerStart();
+    getFooter1();
+    getFooter2();
+    menuKort()
 }
 
 
@@ -28,34 +28,34 @@ function start() {
 //*-----------------------------Burgermenu sektion-----------------------------------*//
 
 function burgerStart() {
-	document.querySelector(".menu-button").addEventListener("click", openBurger());
-	document.querySelector(".menu-button").textContent = "☰";
-	document.querySelector(".burgernav").style.display = "none";
+    document.querySelector(".menu-button").addEventListener("click", openBurger());
+    document.querySelector(".menu-button").textContent = "☰";
+    document.querySelector(".burgernav").style.display = "none";
 }
 
 function openBurger() {
-	console.log("open burger");
-	document.querySelector(".burgernav").style.display = "block";
-	document.querySelector(".burgernav").style.display = "grid";
-	document.querySelector(".menu-burger").style.display = "contents";
-	document.querySelector(".fest-burger").style.display = "contents";
-	document.querySelector(".kontakt-burger").style.display = "contents";
+    console.log("open burger");
+    document.querySelector(".burgernav").style.display = "block";
+    document.querySelector(".burgernav").style.display = "grid";
+    document.querySelector(".menu-burger").style.display = "contents";
+    document.querySelector(".fest-burger").style.display = "contents";
+    document.querySelector(".kontakt-burger").style.display = "contents";
 
-	document.querySelector(".menu-button").addEventListener("click", closeBurger);
-	document.querySelector(".menu-button").removeEventListener("click", openBurger);
-	document.querySelector(".menu-button").textContent = "⤫";
+    document.querySelector(".menu-button").addEventListener("click", closeBurger);
+    document.querySelector(".menu-button").removeEventListener("click", openBurger);
+    document.querySelector(".menu-button").textContent = "⤫";
 }
 
 function closeBurger() {
-	console.log("close burger");
-	document.querySelector(".burgernav").style.display = "none";
-	document.querySelector(".menu-burger").style.display = "none";
-	document.querySelector(".fest-burger").style.display = "none";
-	document.querySelector(".kontakt-burger").style.display = "none";
+    console.log("close burger");
+    document.querySelector(".burgernav").style.display = "none";
+    document.querySelector(".menu-burger").style.display = "none";
+    document.querySelector(".fest-burger").style.display = "none";
+    document.querySelector(".kontakt-burger").style.display = "none";
 
-	document.querySelector(".menu-button").removeEventListener("click", closeBurger);
-	document.querySelector(".menu-button").addEventListener("click", openBurger);
-	document.querySelector(".menu-button").textContent = "☰";
+    document.querySelector(".menu-button").removeEventListener("click", closeBurger);
+    document.querySelector(".menu-button").addEventListener("click", openBurger);
+    document.querySelector(".menu-button").textContent = "☰";
 }
 
 //*-----------------------------Burgermenu sektion SLUT-----------------------------------*//
@@ -69,19 +69,19 @@ function closeBurger() {
 //------------------------------------Menu sektion ----------------------------------------
 
 async function menuKort() {
-	console.log("menukort");
+    console.log("menukort");
 
-	let pagesURL = "http://widespace.dk/MET-WP/wp-json/wp/v2/menu/42";
-	let jsonData = await fetch(pagesURL);
-	let section = await jsonData.json();
-	insertmenuKort(section);
+    let pagesURL = "http://widespace.dk/MET-WP/wp-json/wp/v2/menu/42";
+    let jsonData = await fetch(pagesURL);
+    let section = await jsonData.json();
+    insertmenuKort(section);
 }
 
 function insertmenuKort(section) {
-	let destmenuKort = document.querySelector("#menukort");
+    let destmenuKort = document.querySelector("#menukort");
 
-	let template =
-		`
+    let template =
+        `
 
      	<div id="coffee_section">
 		<div class="menu-left">
@@ -90,6 +90,7 @@ function insertmenuKort(section) {
 				<p>${section.kaffe_beskrivelse}</p>
 				<div class="white-border1"></div>
 			</div>
+
 
 			<div class="coffee-list">
 				<ul>
@@ -132,12 +133,10 @@ function insertmenuKort(section) {
 					<li>
 						<p>${section.kaffe10}</p>
 					</li>
-
 				</ul>
 
 
 				<ul>
-
 					<li>
 						<p>${section.kaffe1_pris}</p>
 					</li>
@@ -178,10 +177,10 @@ function insertmenuKort(section) {
 						<p>${section.kaffe10_pris}</p>
 					</li>
 
-
-
 				</ul>
 			</div>
+
+
 
 		</div>
 			<div class="menu-right">
@@ -189,58 +188,183 @@ function insertmenuKort(section) {
 			</div>
 		</div>
 
-		<div id="morgenmad-section">
-			<div class="menu-left">
-				<img src="${section.morgenmadbillede.guid}" alt="Billede til: ${section.title.rendered}";>
+
+
+
+
+
+	<div id="morgenmad-section">
+		<div class="menu-left">
+			<div class="morgenmad-text">
+				<h1>${section.morgenmad_overskrift}</h1>
+				<p>${section.morgenmad_beskrivelse}</p>
 				<div class="white-border2"></div>
 			</div>
+
+			<div class="morgenmad-list">
+				<ul>
+					<li>
+						<p>${section.morgenmad1}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad2}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad3}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad4}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad5}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad6}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad7}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad8}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad9}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad10}</p>
+					</li>
+
+				</ul>
+
+
+				<ul>
+
+					<li>
+						<p>${section.morgenmad1_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad2_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad3_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad4_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad5_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad6_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad7_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad8_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad9_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.morgenmad10_pris}</p>
+					</li>
+
+
+
+				</ul>
+			</div>
+
+		</div>
 			<div class="menu-right">
-
-				<div class="morgen-text">
-					<h1>${section.morgenmad_overskrift}</h1>
-					<p>${section.morgenmad_beskrivelse}</p>
-				</div>
-				<div class="morgen-list">
-					<ul>
-						<li>
-							<p>${section.morgenmad_menu}</p>
-							<p>${section.morgenmad_priser}</p>
-						</li>
-					</ul>
-				</div>
-
+				<img src="${section.morgenmadbillede.guid}" alt="Billede til: ${section.title.rendered}" ;>
 			</div>
 		</div>
 
 
-		<div id="koldedrikke-section">
-			<div class="menu-left">
-				<div class="drikke-text">
-					<h1>${section.kolde_drikke}</h1>
-					<p>${section.kolde_beskrivelse}</p>
-				</div>
-				<div class="drikke-list">
-					<ul>
-						<li>
-							<p>${section.kolde_menu}</p>
-							<p>${section.kolde_priser}</p>
-						</li>
-					</ul>
-				</div>
+
+
+
+
+
+	<div id="koldedrikke-section">
+		<div class="menu-left">
+			<div class="drikke-text">
+				<h1>${section.kolde_drikke}</h1>
+				<p>${section.kolde_beskrivelse}</p>
+				<div class="white-border2"></div>
 			</div>
 
+			<div class="drikke-list">
+				<ul>
+					<li>
+						<p>${section.koldedrikke1}</p>
+					</li>
+
+					<li>
+						<p>${section.koldedrikke2}</p>
+					</li>
+
+					<li>
+						<p>${section.koldedrikke3}</p>
+					</li>
+
+					<li>
+						<p>${section.koldedrikke4}</p>
+					</li>
+
+				</ul>
+
+
+				<ul>
+
+					<li>
+						<p>${section.koldedrikke1_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.koldedrikke2_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.koldedrikke3_pris}</p>
+					</li>
+
+					<li>
+						<p>${section.koldedrikke4_pris}</p>
+					</li>
+
+				</ul>
+			</div>
+
+		</div>
 			<div class="menu-right">
-				<img src="${section.koldebillede.guid}" alt="Billede til: ${section.title.rendered}";>
-					<div class="white-border3"></div>
-
+				<img src="${section.koldebillede.guid}" alt="Billede til: ${section.title.rendered}" ;>
 			</div>
-
 		</div>
 
 
 					`;
 
-	destmenuKort.insertAdjacentHTML("beforeend", template);
+    destmenuKort.insertAdjacentHTML("beforeend", template);
 }
 
 //---------------------------------Menu sektion SLUT ----------------------------
@@ -256,46 +380,46 @@ function insertmenuKort(section) {
 /* aabningstider og dage */
 
 async function getFooter1() {
-	console.log("get left");
-	let pagesURL = "http://widespace.dk/MET-WP/wp-json/wp/v2/footer/125";
-	let jsonData = await fetch(pagesURL);
-	let section = await jsonData.json();
-	insertFooter1(section);
+    console.log("get left");
+    let pagesURL = "http://widespace.dk/MET-WP/wp-json/wp/v2/footer/125";
+    let jsonData = await fetch(pagesURL);
+    let section = await jsonData.json();
+    insertFooter1(section);
 }
 
 function insertFooter1(section) {
-	let destFooter1 = document.querySelector(".footer-left");
+    let destFooter1 = document.querySelector(".footer-left");
 
-	let template =
-		`
+    let template =
+        `
 				<p>${section.aabningsdage}</p>
                 <p>${section.aabningsdage_2}</p>
 					`;
-	destFooter1.insertAdjacentHTML("beforeend", template);
+    destFooter1.insertAdjacentHTML("beforeend", template);
 
 }
 
 /* adresse og information */
 
 async function getFooter2() {
-	console.log("get right");
-	let pagesURL = "http://widespace.dk/MET-WP/wp-json/wp/v2/footer/125";
-	let jsonData = await fetch(pagesURL);
-	let section = await jsonData.json();
-	insertFooter2(section);
+    console.log("get right");
+    let pagesURL = "http://widespace.dk/MET-WP/wp-json/wp/v2/footer/125";
+    let jsonData = await fetch(pagesURL);
+    let section = await jsonData.json();
+    insertFooter2(section);
 }
 
 function insertFooter2(section) {
-	let destFooter2 = document.querySelector(".footer-right");
+    let destFooter2 = document.querySelector(".footer-right");
 
-	let template =
-		`
+    let template =
+        `
                <p>${section.adresse_footer}</p>
                <p>${section.telefonnummer}</p>
                <p>${section.mail_footer}</p>
 
 					`;
-	destFooter2.insertAdjacentHTML("beforeend", template);
+    destFooter2.insertAdjacentHTML("beforeend", template);
 
 }
 
